adbc49d1ba30d1ab5b564c13e54f3751d59183d4
davronbek <kamolahasanova@gmail.com>
2024-06-13 22:55:20 +0500

    added

--- App.ts
+++ App.ts
@@ -1,13 +1,40 @@
-var inputName = document.querySelector("#name") as HTMLInputElement;
-var inputAge = document.querySelector("#age") as HTMLInputElement;
-var inputSurName = document.querySelector("#surName") as HTMLInputElement;
-var btn = document.querySelector("button") as HTMLButtonElement;
-var ism = document.querySelector("#ism");
-var yosh = document.querySelector("#yosh");
-var familiya = document.querySelector("#familiya");
+interface User {
+  name: string;
+  familiya: string;
+  yosh: number;
+}
 
-btn.addEventListener("click", () => {
-  ism.textContent = inputName.value;
-  yosh.textContent = inputAge.value;
-  familiya.textContent = inputSurName.value;
+const users: User[] = [];
+
+const userForm = document.getElementById("userForm") as HTMLFormElement;
+const nameInput = document.getElementById("name") as HTMLInputElement;
+const surNameInput = document.getElementById("surName") as HTMLInputElement;
+const yoshInput = document.getElementById("age") as HTMLInputElement;
+const cardContainer = document.getElementById(
+  "cardContainer"
+) as HTMLDivElement;
+
+userForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const newUser: User = {
+    name: nameInput.value,
+    familiya: surNameInput.value,
+    yosh: yoshInput.value,
+  };
+  users.push(newUser);
+  nameInput.value = "";
+  surNameInput.value = "";
+  yoshInput.value = "";
+  displayUsers();
 });
+
+function displayUsers() {
+  cardContainer.innerHTML = "";
+  users.forEach((user) => {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `<h2>${user.name}</h2><h2>${user.familiya}</h2><h2>${user.yosh}</h2>
+    `;
+    cardContainer.appendChild(card);
+  });
+}
